706c0efb2d2561521a70b198c2e335ef89c5843c
namanbajpaikrr@gmail.com <namanbajpaikrr@gmail.com>
2024-05-03 23:02:20 +0530

    first change to check

--- Scripts/ARUIManager.cs
+++ Scripts/ARUIManager.cs
@@ -22,6 +22,7 @@ public class ARUIManager : MonoBehaviour
 
     private void Start()
     {
+        print("namanm");
         deassembleBtn.onClick.AddListener(() => 
         {
             arManager.ExplosionModel.DismantleMachine();
